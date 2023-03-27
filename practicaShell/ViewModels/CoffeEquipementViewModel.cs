a9268c58de3f1df2ed0ca5686f67de5f3f7bee73
Jorge <jorge.ricardo@utp.edu.co>
2023-03-27 09:25:24 -0500

    Agregar archivos de proyecto.

---
+++ practicaShell/ViewModels/CoffeEquipementViewModel.cs
@@ -0,0 +1,125 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using practicaShell.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Command = MvvmHelpers.Commands.Command;
+
+namespace practicaShell.ViewModels
+{
+    public class CoffeEquipementViewModel : BaseViewModel
+    {
+        
+
+        public ObservableCollection<Coffee> Coffee { get; set; }
+        public ObservableRangeCollection<Grouping<string, Coffee>> CoffeGroups { get;  }
+        public AsyncCommand RefreshCommand { get;}
+        public AsyncCommand<Coffee> FavoriteCommand { get;}
+        public Command LoadMoreCommand { get;}
+        public Command DelayLoadMoreCommand { get; }
+        public Command ClearCommand { get; }
+
+        public CoffeEquipementViewModel()
+        {
+            Title = "Coffe equipement";
+
+            Coffee = new ObservableCollection<Coffee>();
+            CoffeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+
+            var image = "https://i.ibb.co/VNjKrpG/coffe-Image.jpg";
+
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue bottle", Name = "kenya Kiambu", Image = image });
+
+            CoffeGroups.Add(new Grouping<string, Coffee>("Blue bottle", new[] { Coffee[2] }));
+            CoffeGroups.Add(new Grouping<string, Coffee>("Yes Pls", Coffee.Take(2)));
+
+            RefreshCommand = new AsyncCommand(Refresh);
+            FavoriteCommand = new AsyncCommand<Coffee>(Favorite);
+            LoadMoreCommand = new Command(LoadMore);
+            DelayLoadMoreCommand = new Command(DelayLoadMore);
+            ClearCommand = new Command(Clear);
+        }
+
+        Coffee _previouslySelected;
+        Coffee _selectedCoffe;
+
+        async Task Favorite(Coffee coffee)
+        {
+            if (coffee == null)
+            {
+                return;
+            }
+            await Application.Current.MainPage.DisplayAlert("Favorite",coffee.Name, "Ok");
+        }
+        public Coffee SelectedCoffe
+        {
+            get => _selectedCoffe;
+            set
+            {
+                if (value != null)
+                {
+                    Application.Current.MainPage.DisplayAlert("Selected", value.Name, "Ok");
+                    _previouslySelected = value;
+                    value = null;
+                }
+                _selectedCoffe = value;
+                OnPropertyChanged();
+            }
+        }
+
+        void LoadMore()
+        {
+            if (Coffee.Count >= 15)
+            {
+                return;
+            }
+            var image = "https://i.ibb.co/VNjKrpG/coffe-Image.jpg";
+
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue bottle", Name = "kenya Kiambu", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue bottle", Name = "kenya Kiambu", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue bottle", Name = "kenya Kiambu", Image = image });
+            CoffeGroups.Clear();
+
+            CoffeGroups.Add(new Grouping<string, Coffee>("Blue bottle", Coffee.Where(c => c.Roaster == "Blue bottle")));
+            CoffeGroups.Add(new Grouping<string, Coffee>("Yes Pls", Coffee.Where(c => c.Roaster == "Yes Plz")));
+        }
+
+        async Task Refresh()
+        {
+            IsBusy = true;
+
+            await Task.Delay(2000);
+            Coffee.Clear();
+            LoadMore();
+
+            IsBusy = false;
+        }
+
+        void DelayLoadMore()
+        {
+            if (Coffee.Count < 10)
+            {
+                return;
+            }
+            LoadMore();
+        }
+
+        void Clear()
+        {
+            Coffee.Clear();
+            CoffeGroups.Clear();
+        }
+    }
+}
---
+++ practicaShell/Views/CoffeEquipementPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using practicaShell.Models;
+using practicaShell.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace practicaShell.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoffeEquipementPage : ContentPage
+    {
+        public CoffeEquipementPage()
+        {
+            InitializeComponent();
+            
+            //BindingContext = new CoffeEquipementViewModel();
+            //LabelCount.Text = "hello from code behind";
+            
+        }
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var coffe = ((ListView)sender).SelectedItem as Coffee;
+            if (coffe == null)
+            {
+                return;
+            }
+            await DisplayAlert("Coffe selected", coffe.Name, "Ok");
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var coffe = ((MenuItem)sender).BindingContext as Coffee;
+            if (coffe == null)
+            {
+                return;
+            }
+            await DisplayAlert("Coffe favorited", coffe.Name, "Ok");
+        }
+    }
+}
